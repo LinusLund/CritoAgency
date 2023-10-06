205a39b8710fd7bbbff0b51d5a3882fec940ffd6
LinusLund <lundlinus@live.se>
2023-10-06 11:43:09 +0200

    Add project files.

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,40 @@
+﻿using CritoAgency.Models;
+using CritoAgency.Services;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace CritoAgency.Controllers
+{
+    public class ContactsController : SurfaceController
+    {
+        public ContactsController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactForm contactForm)
+        {
+            if (!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+            //Send email
+            using var mail = new MailService("no-reply@always-reply.com","hjalpmighjalp@websupport.se", 671, "umbraco.Linus@se.com","********");
+
+            //To Sender
+            mail.SendAsync(contactForm.Email, "Your Request was recieved", "Hi, your request was recieved and we wont be contacting you").ConfigureAwait(false);
+
+            //Reciever
+            mail.SendAsync("orreBorre@umbraco.com",$"{contactForm.Name} sent a request", contactForm.Message).ConfigureAwait(false);
+
+
+
+            return LocalRedirect(contactForm.RedirectUrl ?? "/");
+        }
+    }
+}
---
+++ Models/ContactForm.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack.Formatters;
+using System.ComponentModel.DataAnnotations;
+
+namespace CritoAgency.Models
+{
+    public class ContactForm
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Message { get; set; } = null!;
+
+        public string? RedirectUrl { get; set; } = "/";
+
+    }
+}
